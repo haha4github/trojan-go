995fcf36f25b73879d88bcc374dc3b3347f20578
haha4github <55265840+haha4github@users.noreply.github.com>
2023-03-05 23:54:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 
-	_ "github.com/p4gefau1t/trojan-go/component"
+	_ "github.com/haha4github/trojan-go/component"
 	"github.com/p4gefau1t/trojan-go/log"
 	"github.com/p4gefau1t/trojan-go/option"
 )
