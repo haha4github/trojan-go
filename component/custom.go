b9018d754ecfa55f77695ada3210d5dde5d2d16f
haha4github <55265840+haha4github@users.noreply.github.com>
2023-03-06 00:21:42 +0800

    Update custom.go

--- component/custom.go
+++ component/custom.go
@@ -4,6 +4,7 @@
 package build
 
 import (
+	_ "github.com/haha4github/trojan-go/tunnel/trojan"
 	_ "github.com/p4gefau1t/trojan-go/proxy/custom"
 	_ "github.com/p4gefau1t/trojan-go/tunnel/adapter"
 	_ "github.com/p4gefau1t/trojan-go/tunnel/dokodemo"
@@ -17,6 +18,5 @@ import (
 	_ "github.com/p4gefau1t/trojan-go/tunnel/tls"
 	_ "github.com/p4gefau1t/trojan-go/tunnel/tproxy"
 	_ "github.com/p4gefau1t/trojan-go/tunnel/transport"
-	_ "github.com/p4gefau1t/trojan-go/tunnel/trojan"
 	_ "github.com/p4gefau1t/trojan-go/tunnel/websocket"
 )
